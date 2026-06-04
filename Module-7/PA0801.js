2569735db0035b8e0997fa21c18f8844bbee16c0
Mojalefa <mojalefamaarman20@gmail.com>
2026-06-04 09:50:46 +0200

    Add files via upload

---
+++ Module-7/PA0801.js
@@ -0,0 +1,9 @@
+//create a withdrawal and deposite function for a bank account. 
+//Purpose is to show how functions can be used to perform specific tasks and how they can interact with variables to manipulate data.
+var Balance = 100;
+var latestBalance;
+
+function withdrawal(debited) {
+  
+}
+
